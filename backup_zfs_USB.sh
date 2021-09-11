f8be8aa0ac26711e3ee5f43c272c920ab910cc5f
Pierre <pierre.peyronnel@gmail.com>
2021-09-11 20:00:48 +0200

    Removed clean up step, leave for dedicated script

--- backup_zfs_USB.sh
+++ backup_zfs_USB.sh
@@ -34,7 +34,6 @@ LOGFILE=${LOGDIR}/backup.`date +%Y%m%d.%H%M`.log
 # MAIN 
 ##########
 
-clean_logs
 log "*******************************"
 log "Starting ZFS backup of $SOURCE on `date '+%Y%m%d-%H%M'`"
 
@@ -52,14 +51,7 @@ fi
 log "Starting the backup"
 ##${ZFS} send -R -I ${SOURCE}@${PREVIOUS} ${SOURCE}@${TODAY} | ssh root@$REMOTE_BACKUP_HOST ${ZFS} receive -Fduv $REMOTE_BACKUP_POOL
 ${ZFS} send -R -I ${SOURCE}@${PREVIOUS} ${SOURCE}@${TODAY} | ${ZFS} receive -Fduv ${BACKUP_POOL} >> $LOGFILE 2>&1
-
-# Cleanup of snapshots on $SOURCE based on backups
-log " Backup complete, destroying previous snapshot (if it's not the last one...)"
-NBR_BACKUP=`${ZFS} list -r -H -t snapshot ${BACKUP_POOL} | grep "${BACKUP_POOL}\@" | wc -l`
-if [ ${NBR_BACKUP} -gt 1 ]
-then 
-  ${ZFS} destroy -r ${SOURCE}@${PREVIOUS} || fatal "cannot destroy previous snapshot aborting"
-fi
+log " Backup complete"
 
 log "End of backup"
 
