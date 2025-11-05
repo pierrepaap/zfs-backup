72365839a04ff0a54d4dfcdf9c79aaae813e2155
pierrepaap <pierre.peyronnel@gmail.com>
2025-11-05 12:23:50 +0100

    adding logging for the ZFS command

--- backup_zfs_remote.sh
+++ backup_zfs_remote.sh
@@ -63,8 +63,8 @@ do
 
   # transfer to remote host
   log " Starting the transfer"
-  log "${ZFS} send -R -I ${SOURCE_FS}@${PREVIOUS} ${SOURCE_FS}@${NOW} | ssh backup@${REMOTE_BACKUP_HOST} -i ${KEY} sudo ${ZFS} receive -Fduv ${BACKUP_POOL}"
-  ${ZFS} send -R -I ${SOURCE_FS}@${PREVIOUS} ${SOURCE_FS}@${NOW} | ssh backup@${REMOTE_BACKUP_HOST} -i ${KEY} sudo ${ZFS} receive -Fduv ${BACKUP_POOL}
+  log "${ZFS} send -R -I ${SOURCE_FS}@${PREVIOUS} ${SOURCE_FS}@${NOW} | ssh backup@${REMOTE_BACKUP_HOST} -i ${KEY} sudo ${ZFS} receive -Fduv ${BACKUP_POOL} >> $LOGFILE 2>&1"
+  ${ZFS} send -R -I ${SOURCE_FS}@${PREVIOUS} ${SOURCE_FS}@${NOW} | ssh backup@${REMOTE_BACKUP_HOST} -i ${KEY} sudo ${ZFS} receive -Fduv ${BACKUP_POOL} >> $LOGFILE 2>&1
   ##${ZFS} send -R -I ${SOURCE}@${PREVIOUS} ${SOURCE}@${NOW} | ${ZFS} receive -Fduv ${BACKUP_POOL} >> $LOGFILE 2>&1
   log " Transfer complete for ${SOURCE_FS}"
   log "    **************************"
